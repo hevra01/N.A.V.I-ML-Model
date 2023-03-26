d3de5c4d9effea27629ab53ec95d328552be8774
Hevra Petekkaya <hevrapetekkaya1@gmail.com>
2023-03-26 15:29:14 +0300

    finished the loss class

--- cost.py
+++ cost.py
@@ -44,12 +44,36 @@ class YoloLoss(nn.Module):
             ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
             object_loss = self.mse(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
 
+            # ======================== #
+            #   FOR BOX COORDINATES    #
+            # ======================== #
+
+            predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])  # x,y coordinates
+            target[..., 3:5] = torch.log(
+                (1e-16 + target[..., 3:5] / anchors)
+            )  # width, height coordinates
+            box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
+
             # ================== #
             #   FOR CLASS LOSS   #
             # ================== #
 
             class_loss = self.entropy(
                 (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
+            )
+
+            # print("__________________________________")
+            # print(self.lambda_box * box_loss)
+            # print(self.lambda_obj * object_loss)
+            # print(self.lambda_noobj * no_object_loss)
+            # print(self.lambda_class * class_loss)
+            # print("\n")
+
+            return (
+                    self.lambda_box * box_loss
+                    + self.lambda_obj * object_loss
+                    + self.lambda_noobj * no_object_loss
+                    + self.lambda_class * class_loss
             )
 
 
