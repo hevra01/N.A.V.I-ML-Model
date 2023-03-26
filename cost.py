f83aaf1446f4dd134c5d6a6b75df435f90d7be44
Hevra Petekkaya <hevrapetekkaya1@gmail.com>
2023-03-26 15:24:56 +0300

    defined the loss for anchors that predicted an object to exist

--- cost.py
+++ cost.py
@@ -32,5 +32,16 @@ class YoloLoss(nn.Module):
                 (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj]),
             )
 
+            # ==================== #
+            #   FOR OBJECT LOSS    #
+            # ==================== #
+
+            anchors = anchors.reshape(1, 3, 1, 1, 2)
+            box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors],
+                                  dim=-1)
+            ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
+            object_loss = self.mse(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
+
+
 
 
