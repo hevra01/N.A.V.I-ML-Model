0367809f747da33f076be03be276f3dee899e04b
Hevra Petekkaya <hevrapetekkaya1@gmail.com>
2023-03-27 12:43:44 +0300

    the sanity test for cost has been implemented but there is a problem with it

--- cost.py
+++ cost.py
@@ -47,6 +47,7 @@ def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
 
     return intersection / (box1_area + box2_area - intersection + 1e-6)
 
+
 class YoloLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -62,64 +63,68 @@ class YoloLoss(nn.Module):
         self.lambda_box = 10
         # change: define a loss for distance as well.
 
-        def forward(self, predictions, target, anchors):
-            # Check where obj and noobj (we ignore if target == -1)
-            obj = target[..., 0] == 1  # in paper this is Iobj_i
-            noobj = target[..., 0] == 0  # in paper this is Inoobj_i
+    def forward(self, predictions, target, anchors):
+        # Check where obj and noobj (we ignore if target == -1)
+        obj = target[..., 0] == 1  # in paper this is Iobj_i
+        print("obj: ", obj)
+        noobj = target[..., 0] == 0  # in paper this is Inoobj_i
 
-            # ======================= #
-            #   FOR NO OBJECT LOSS
-            #   This is to penalize anchors that didn't predict the existence of an object
-            #   by only the objectness loss and not misclassification or bounding box loss
-            # ======================= #
+        print("noobj: ", noobj)
 
-            no_object_loss = self.bce(
-                (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj]),
-            )
+        # ======================= #
+        #   FOR NO OBJECT LOSS
+        #   This is to penalize anchors that didn't predict the existence of an object
+        #   by only the objectness loss and not misclassification or bounding box loss
+        # ======================= #
 
-            # ==================== #
-            #   FOR OBJECT LOSS
-            #   This is for anchors that predicted that an object exists.
-            #   The loss will include IOU (intersection over union)#
-            # ==================== #
+        no_object_loss = self.bce(
+            (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj]),
+        )
 
-            anchors = anchors.reshape(1, 3, 1, 1, 2)
-            box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors],
-                                  dim=-1)
-            ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
-            object_loss = self.mse(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
+        # ==================== #
+        #   FOR OBJECT LOSS
+        #   This is for anchors that predicted that an object exists.
+        #   The loss will include IOU (intersection over union)#
+        # ==================== #
 
-            # ======================== #
-            #   FOR BOX COORDINATES    #
-            # ======================== #
+        anchors = anchors.reshape(1, 3, 1, 1, 2)
+        box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors],
+                              dim=-1)
+        ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
+        print("predictions[..., 0:1][obj]: ", predictions[..., 0:1][obj])
+        object_loss = self.mse(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
 
-            predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])  # x,y coordinates
-            target[..., 3:5] = torch.log(
-                (1e-16 + target[..., 3:5] / anchors)
-            )  # width, height coordinates
-            box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
+        # ======================== #
+        #   FOR BOX COORDINATES    #
+        # ======================== #
 
-            # ================== #
-            #   FOR CLASS LOSS   #
-            # ================== #
+        predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])  # x,y coordinates
+        target[..., 3:5] = torch.log(
+            (1e-16 + target[..., 3:5] / anchors)
+        )  # width, height coordinates
+        box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
 
-            class_loss = self.entropy(
-                (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
-            )
+        # ================== #
+        #   FOR CLASS LOSS   #
+        # ================== #
 
-            # print("__________________________________")
-            # print(self.lambda_box * box_loss)
-            # print(self.lambda_obj * object_loss)
-            # print(self.lambda_noobj * no_object_loss)
-            # print(self.lambda_class * class_loss)
-            # print("\n")
+        class_loss = self.entropy(
+            (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
+        )
 
-            return (
-                    self.lambda_box * box_loss
-                    + self.lambda_obj * object_loss
-                    + self.lambda_noobj * no_object_loss
-                    + self.lambda_class * class_loss
-            )
+        # print("__________________________________")
+        # print(self.lambda_box * box_loss)
+        # print(self.lambda_obj * object_loss)
+        # print(self.lambda_noobj * no_object_loss)
+        # print(self.lambda_class * class_loss)
+        # print("\n")
+
+        return (
+                self.lambda_box * box_loss
+                + self.lambda_obj * object_loss
+                + self.lambda_noobj * no_object_loss
+                + self.lambda_class * class_loss
+        )
 
 
 
--- cost_sanity_check.py
+++ cost_sanity_check.py
@@ -1,9 +1,17 @@
 import torch
 from cost import YoloLoss
+from cost import intersection_over_union
 
-# Create a dummy input tensor for testing purposes
-predictions = torch.randn((3, 7, 7, 5))
-target = torch.randn((3, 7, 7, 6))
+img_size = 256 # this value has randomly been assigned
+num_classes = 7 # num of classes in kitti dataset
+
+
+# Create a dummy tensors for testing purposes
+# 2 represents the batch size and 3 represents the anchor number
+predictions = torch.randn((2, 3, img_size//32, img_size//32, 5 + num_classes))
+#predictions[..., 0] = 1
+target = torch.randn((2, 3, img_size//32, img_size//32, 5 + num_classes))
+#target[..., 0] = 1
 anchors = torch.tensor([(1, 1), (2, 2), (3, 3)])
 
 # Initialize the YoloLoss module
@@ -14,3 +22,20 @@ loss = loss_fn(predictions, target, anchors)
 
 # Check that loss is finite
 assert torch.isfinite(loss), f"Loss has NaN or infinite values."
+
+# Test intersection_over_union function
+box1 = torch.tensor([0.5, 0.5, 1.0, 1.0])
+box2 = torch.tensor([0.6, 0.6, 1.2, 1.2])
+iou = intersection_over_union(box1, box2)
+assert iou >= 0 and iou <= 1, "intersection_over_union function is not returning a valid value"
+
+# Verify that YoloLoss module has all the required attributes and methods
+assert hasattr(loss_fn, 'mse'), "mse attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'bce'), "bce attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'entropy'), "entropy attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'sigmoid'), "sigmoid attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'lambda_class'), "lambda_class attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'lambda_noobj'), "lambda_noobj attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'lambda_obj'), "lambda_obj attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'lambda_box'), "lambda_box attribute is not defined in the YoloLoss module"
+assert hasattr(loss_fn, 'forward'), "forward method is not defined in the YoloLoss module"
